85734873e6a5281419496b70744643c854cb12f9
Daniela Akerman <daniela.akerman@mi.unc.edu.ar>
2023-02-23 13:44:58 -0300

    Remodelando el front

--- front-src/index.ts
+++ front-src/index.ts
@@ -1,14 +1,10 @@
-import { initRouter } from "./router";
+// import { initRouter } from "./router";
 import { state } from "./state";
 import "./components/navbar"
-import "./components/button"
-import "./components/input"
-import "./components/textarea"
-import "./components/text"
-import "./components/card"
 
 
-(function () {
-  const root = document.querySelector(".root") as HTMLElement;
-  initRouter(root);
-})();
+
+// (function () {
+//   const root = document.querySelector(".root") as HTMLElement;
+//   initRouter(root);
+// })();
--- front-src/router.ts
+++ front-src/router.ts
@@ -1,65 +1,65 @@
-import { initPageHome } from "./pages/1-home";
-import { initPageIngresar } from "./pages/2-ingresar";
-import { initPagePassword } from "./pages/3-password";
-import { initPageDatos } from "./pages/4-mis-datos";
-import { initPageReport } from "./pages/5-reportar";
-import { initPageEditar } from "./pages/6-editar";
-import { initPageMascotas } from "./pages/7-mis-mascotas";
+// import { initPageHome } from "./pages/1-home";
+// import { initPageIngresar } from "./pages/2-ingresar";
+// import { initPagePassword } from "./pages/3-password";
+// import { initPageDatos } from "./pages/4-mis-datos";
+// import { initPageReport } from "./pages/5-reportar";
+// import { initPageEditar } from "./pages/6-editar";
+// import { initPageMascotas } from "./pages/7-mis-mascotas";
 
 
-const routes = [
-  {
-    path: /\//,
-    handler: initPageHome,
-  },
-  {
-    path: /\/reportar/,
-    handler: initPageReport,
-  },
-  {
-    path: /\/ingresar/,
-    handler: initPageIngresar,
-  },
-  {
-    path: /\/password/,
-    handler: initPagePassword,
-  },
-  {
-    path: /\/mis-datos/,
-    handler: initPageDatos,
-  },
-  {
-    path: /\/editar/,
-    handler: initPageEditar,
-  },
-  {
-    path: /\/mis-mascotas/,
-    handler: initPageMascotas,
-  }
-];
+// const routes = [
+//   {
+//     path: /\//,
+//     handler: initPageHome,
+//   },
+//   {
+//     path: /\/reportar/,
+//     handler: initPageReport,
+//   },
+//   {
+//     path: /\/ingresar/,
+//     handler: initPageIngresar,
+//   },
+//   {
+//     path: /\/password/,
+//     handler: initPagePassword,
+//   },
+//   {
+//     path: /\/mis-datos/,
+//     handler: initPageDatos,
+//   },
+//   {
+//     path: /\/editar/,
+//     handler: initPageEditar,
+//   },
+//   {
+//     path: /\/mis-mascotas/,
+//     handler: initPageMascotas,
+//   }
+// ];
 
-export function initRouter(container: Element) {
-  function goTo(path) {
-    history.pushState({}, "", path);
-    handleRoute(path);
-  }
+// export function initRouter(container: Element) {
+//   function goTo(path) {
+//     history.pushState({}, "", path);
+//     handleRoute(path);
+//   }
 
-  function handleRoute(route) {
-    for (const r of routes) {
-      if (r.path.test(route)) {
-        const el = r.handler({ goTo });
+//   function handleRoute(route) {
+//     for (const r of routes) {
+//       if (r.path.test(route)) {
+//         const el = r.handler({ goTo });
 
-        if (container.firstChild) {
-          container.firstChild.remove();
-        }
-        container.appendChild(el);
-      }
-    }
-  }
+//         if (container.firstChild) {
+//           container.firstChild.remove();
+//         }
+//         container.appendChild(el);
+//       }
+//     }
+//   }
 
-  handleRoute(location.pathname);
+//   handleRoute(location.pathname);
 
-  window.onpopstate = function () {
-    handleRoute(location.pathname);
-  };
-}
+//   window.onpopstate = function () {
+//     handleRoute(location.pathname);
+//   };
+// }
