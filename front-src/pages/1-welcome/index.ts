fad4ae87f00ec9ba3be473e85473bee3b1d39d79
Daniela Akerman <daniela.akerman@mi.unc.edu.ar>
2023-02-24 12:20:15 -0300

    Listo reportes, funciona OK

--- front-src/pages/1-welcome/index.ts
+++ front-src/pages/1-welcome/index.ts
@@ -62,100 +62,41 @@ export function initPageWelcome(root) {
         </div>
       </div>
     </div>
-
-        
   `;
   const contenedor = div.querySelector(".results");
-
   const template = div.querySelector("#template");
-
   const myModal = div.querySelector(".modal")!;
+  const form = div.querySelector(".form")!;
 
   myModal.addEventListener("shown.bs.modal", (e) => {
-    console.log("escuchando el modal");
     const event = e as any;
     const button = event.relatedTarget;
     const petName = button.getAttribute("data-bs-pet_name");
-    // const petId = button.getAttribute("data-bs-pet_id");
+    const petId = button.getAttribute("data-bs-pet_id");
 
     const modalTitle = myModal.querySelector(".modal-title")!;
+    modalTitle.textContent = `Ayudanos a encontrar a ${petName}`;
 
     const inputReporterName = myModal.querySelector(".name")! as any;
     const inputReporterTelefono = myModal.querySelector(".telefono")! as any;
     const inputReporterMensaje = myModal.querySelector(".mensaje")! as any;
 
-    modalTitle.textContent = `Ayudanos a encontrar a ${petName}`;
-
-    const form = div.querySelector(".form")!;
-    form.addEventListener("submit", (e) => {
+    form.addEventListener("submit", function formulario(e) {
       e.preventDefault();
-
-      // const target = e.target as any;
-      // console.log(target)
-      // const data = new FormData(target);
-      // console.log(data)
-      // const value = Object.fromEntries(data.entries());
-
-
       const reporte: any = {};
 
       reporte.reporter = inputReporterName.value;
       reporte.phone_number = inputReporterTelefono.value;
       reporte.message = inputReporterMensaje.value;
-      reporte.pet_id = button.getAttribute("data-bs-pet_id");
+      reporte.pet_id = petId;
 
-      console.log(reporte)
-      state.setReporte(reporte)
+      state.setReporte(reporte);
+      form.removeEventListener("submit", formulario);
     });
-    (form as any).reset()
+    (form as any).reset();
   });
 
-  // const exampleModal = document.getElementById("exampleModal");
-  // exampleModal.addEventListener("show.bs.modal", (event) => {
-  //   const button = event.relatedTarget;
-  //   const recipient = button.getAttribute("data-bs-whatever");
-  //   const modalTitle = exampleModal.querySelector(".modal-title");
-  //   const modalBodyInput = exampleModal.querySelector(".modal-body input");
-  //   modalTitle.textContent = `New message to ${recipient}`;
-  //   modalBodyInput.value = recipient;
-  // });
-
   state.mostrarMascotasCercaTuyo(root, contenedor, template);
-
-  // let counterB = 2;
-  // const intervalId = setInterval(() => {
-  //   counterB--;
-  //   if (counterB < 0) {
-  //     clearInterval(intervalId);
-
-  //     const form = div.querySelector(".form");
-  //     form!.addEventListener("submit", (e) => {
-  //       e.preventDefault();
-  //       console.log(form);
-
-  //       const target = e.target as any;
-  //       const data = new FormData(target);
-  //       const value = Object.fromEntries(data.entries());
-
-  //       console.log("objeto VALUE", value);
-
-  //       const visto: any = {};
-
-  //       // visto.reporter = div.querySelector(".name")!.value
-  //       // visto.phone_number = div.querySelector(".telefono").value
-  //       // visto.message = div.querySelector(".mensaje").value
-
-  //       const pet_id = div
-  //         .querySelector(".selected_pet")
-  //         ?.getAttribute("pet_id");
-
-  //       visto.pet_id = pet_id;
-
-  //       console.log("objeto VISTO", visto);
-  //       // state.setReporte(visto);
-  //     });
-  //   }
-  // }, 1000);
 
   return div;
 }
