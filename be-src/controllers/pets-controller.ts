9056358d3e830758a545d6401d491d391340d384
Daniela Akerman <daniela.akerman@mi.unc.edu.ar>
2023-03-08 12:22:17 -0300

    Listo MVC

--- be-src/controllers/pets-controller.ts
+++ be-src/controllers/pets-controller.ts
@@ -1,7 +1,7 @@
-import { Pet, User } from "../models";
-
+import { Pet, User, Report } from "../models";
 import { cloudinary } from "../lib/cloudinary";
 import { index } from "../lib/algolia";
+const sgMail = require("@sendgrid/mail");
 
 export async function updateNewPet(updateData) {
   const {
@@ -34,4 +34,113 @@ export async function updateNewPet(updateData) {
     },
   });
   return newPet;
+}
+
+export async function newReport(reportData) {
+  const { reporter, phone_number, message, pet_id, pet_name } = reportData;
+
+  const newReport = await Report.create(reportData);
+
+  const reportedPet = await Pet.findByPk(pet_id);
+  const ownerId = reportedPet.dataValues.userId;
+
+  const owner = await User.findByPk(ownerId);
+  const ownerEmail = owner.dataValues.email;
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: ownerEmail,
+    from: "daniela.akerman@outlook.com",
+    subject: `Nuevo reporte de ${pet_name}`,
+    text: `Hola!`,
+    html: `<strong>LostPets</strong>
+    Hola! Te contamos que ${reporter} vio a tu mascota ${pet_name}, 
+    su número es ${phone_number} 
+    y te dejó este mensaje: ${message}
+    `,
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      return { message: `Reporte enviado a ${ownerEmail}` };
+    })
+    .catch((error) => {
+      console.error(error);
+      return "Error";
+    });
+}
+
+function bodyToIndex(body, id) {
+  const respuesta: any = {};
+  respuesta.objectID = id;
+  if (body.name) {
+    respuesta.name = body.name;
+  }
+  if (body.status) {
+    respuesta.status = body.status;
+  }
+  if (body.ubication) {
+    respuesta.ubication = body.ubication;
+  }
+  if (body.picture_url) {
+    respuesta.picture_url = body.picture_url;
+  }
+  if (body.last_location_lat && body.last_location_lng) {
+    respuesta._geoloc = {
+      lat: body.last_location_lat,
+      lng: body.last_location_lng,
+    };
+  }
+  return respuesta;
+}
+
+export async function editPet(petId, petData) {
+  if (petData.imagen_data) {
+    const { imagen_data } = petData;
+
+    const imagen = await cloudinary.uploader.upload(imagen_data, {
+      resource_type: "image",
+      discard_original_filename: true,
+      width: 1000,
+    });
+    petData.picture_url = imagen.secure_url;
+  }
+
+  const editedPet = await Pet.update(petData, { where: { id: petId } });
+  if (
+    petData.name ||
+    petData.status ||
+    petData.ubication ||
+    petData.picture_url ||
+    (petData.last_location_lat && petData.last_location_lng)
+  ) {
+    const indexItem = bodyToIndex(petData, petId);
+    const algoliaRes = await index.partialUpdateObject(indexItem);
+  }
+  return editedPet;
+}
+
+export async function deletePet(petId, petData) {
+  const deletedPet = await Pet.update(petData, { where: { id: petId } });
+  petData.objectID = petId;
+  const algoliaRes = await index.partialUpdateObject(petData);
+  return deletePet;
+}
+
+export async function getOnePet(petId) {
+  const pet = await Pet.findByPk(petId);
+  return pet;
+}
+
+export async function getMyPets(userId) {
+  const pets = await Pet.findAll({ where: { userId, status: "lost" } });
+  return pets;
+}
+
+export async function petsNearMe(lat, lng){
+const pets = await index.search("lost", {
+  aroundLatLng: [lat, lng].join(","),
+  aroundRadius: 10000,
+});
+return pets
 }
--- be-src/index.ts
+++ be-src/index.ts
@@ -1,22 +1,29 @@
 import * as express from "express";
 import * as path from "path";
 import * as crypto from "crypto";
-import * as jwt from "jsonwebtoken";
 import * as process from "process";
 import * as cors from "cors";
-import { User, Pet, Auth, Report } from "./models";
-import { index } from "./lib/algolia";
-import { cloudinary } from "./lib/cloudinary";
+const sgMail = require("@sendgrid/mail");
+// import * as jwt from "jsonwebtoken";
+// import { User, Pet, Auth, Report } from "./models";
+// import { index } from "./lib/algolia";
+// import { cloudinary } from "./lib/cloudinary";
 import {
   getUserFromToken,
   signUp,
   updateUser,
 } from "./controllers/users-controller";
 import { getAuth, signIn } from "./controllers/auth-controller";
-import { updateNewPet } from "./controllers/pets-controller";
-const sgMail = require("@sendgrid/mail");
-// import {} from "./controllers/users-controller";
-// import {} from "./controllers/auth-controller";
+import {
+  newReport,
+  updateNewPet,
+  editPet,
+  deletePet,
+  getOnePet,
+  getMyPets,
+  petsNearMe,
+} from "./controllers/pets-controller";
+
 
 const port = process.env.PORT;
 const app = express();
@@ -85,122 +92,37 @@ app.post("/new-pet", async (req, res) => {
 });
 
 app.post("/new-report", async (req, res) => {
-  const { reporter, phone_number, message, pet_id, pet_name } = req.body;
-
-  const newReport = await Report.create(req.body);
-
-  const reportedPet = await Pet.findByPk(pet_id);
-  const ownerId = reportedPet.dataValues.userId;
-
-  const owner = await User.findByPk(ownerId);
-  const ownerEmail = owner.dataValues.email;
-
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const msg = {
-    to: ownerEmail,
-    from: "daniela.akerman@outlook.com",
-    subject: `Nuevo reporte de ${pet_name}`,
-    text: `Hola!`,
-    html: `<strong>LostPets</strong>
-    Hola! Te contamos que ${reporter} vio a tu mascota ${pet_name}, 
-    su número es ${phone_number} 
-    y te dejó este mensaje: ${message}
-    `,
-  };
-  sgMail
-    .send(msg)
-    .then(() => {
-      res.json({ message: `Reporte enviado a ${ownerEmail}` });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.json("Error");
-    });
+  const reporte = await newReport(req.body);
+  res.json(reporte);
 });
-
-function bodyToIndex(body, id) {
-  const respuesta: any = {};
-  respuesta.objectID = id;
-  if (body.name) {
-    respuesta.name = body.name;
-  }
-  if (body.status) {
-    respuesta.status = body.status;
-  }
-  if (body.ubication) {
-    respuesta.ubication = body.ubication;
-  }
-  if (body.picture_url) {
-    respuesta.picture_url = body.picture_url;
-  }
-  if (body.last_location_lat && body.last_location_lng) {
-    respuesta._geoloc = {
-      lat: body.last_location_lat,
-      lng: body.last_location_lng,
-    };
-  }
-  return respuesta;
-}
 
 app.post("/edit-pet/:id", async (req, res) => {
   const { id } = req.params;
-
-  if (req.body.imagen_data) {
-    const { imagen_data } = req.body;
-
-    const imagen = await cloudinary.uploader.upload(imagen_data, {
-      resource_type: "image",
-      discard_original_filename: true,
-      width: 1000,
-    });
-    req.body.picture_url = imagen.secure_url;
-  }
-
-  const editedPet = await Pet.update(req.body, { where: { id } });
-  if (
-    req.body.name ||
-    req.body.status ||
-    req.body.ubication ||
-    req.body.picture_url ||
-    (req.body.last_location_lat && req.body.last_location_lng)
-  ) {
-    const indexItem = bodyToIndex(req.body, id);
-    const algoliaRes = await index.partialUpdateObject(indexItem);
-  }
+  const editedPet = await editPet(id, req.body);
   res.json(editedPet);
 });
 
 app.post("/delete-pet/:id", async (req, res) => {
   const { id } = req.params;
-
-  const deletedPet = await Pet.update(req.body, { where: { id } });
-
-  req.body.objectID = id;
-
-  // const indexItem = bodyToIndex(req.body, id);
-  const algoliaRes = await index.partialUpdateObject(req.body);
-
+  const deletedPet = await deletePet(id, req.body);
   res.json(deletedPet);
 });
 
 app.get("/pet/:id", async (req, res) => {
   const { id } = req.params;
-  const pet = await Pet.findByPk(id);
+  const pet = await getOnePet(id);
   res.json(pet);
 });
 
 app.get("/my-pets/:userId", async (req, res) => {
   const { userId } = req.params;
-  const pets = await Pet.findAll({ where: { userId, status: "lost" } });
+  const pets = await getMyPets(userId);
   res.json(pets);
 });
 
 app.get("/pets-near-me", async (req, res) => {
   const { lat, lng } = req.query;
-  const { hits } = await index.search("lost", {
-    aroundLatLng: [lat, lng].join(","),
-    aroundRadius: 10000,
-  });
+  const { hits } = await petsNearMe(lat, lng);
   res.json(hits);
 });
 
